ef431d311660f251848ee589e112d0272b8ed48a
Jan Werren <jan.werren@outlook.com>
2024-01-11 11:15:40 +0100

    feat: day two

---
+++ AdventOfCode2023.Day02/Day02.cs
@@ -0,0 +1,36 @@
+﻿using AdventOfCode2023.Day02;
+
+var content = File.ReadAllLines("input.txt");
+var games = new List<Game>();
+
+foreach (var line in content)
+{
+    var splitted = line.Split(":");
+    var gameId = Convert.ToInt32(splitted[0].Remove(0, 4));
+    var game = new Game
+    {
+        Id = gameId
+    };
+
+    foreach (var takeOut in splitted[1].Split(";"))
+    {
+        foreach (var balls in takeOut.Split(","))
+        {
+            var splittedBalls = balls.Trim().Split(" ");
+            var count = Convert.ToInt32(splittedBalls[0]);
+            var color = splittedBalls[1];
+
+            game.AddIfHigher(color, count);
+        }
+    }
+    
+    games.Add(game);
+}
+
+const int reds = 12;
+const int greens = 13;
+const int blues = 14;
+var validGames = games.Where(g => g.IsValid(reds, greens, blues));
+
+Console.WriteLine(validGames.Sum(g => g.Id));
+Console.WriteLine(games.Sum(g => g.Power));
---
+++ AdventOfCode2023.Day02/Game.cs
@@ -0,0 +1,35 @@
+﻿namespace AdventOfCode2023.Day02;
+
+public class Game
+{
+    public int Id { get; set; }
+    
+    public int Blues { get; set; }
+    
+    public int Greens { get; set; }
+    
+    public int Reds { get; set; }
+
+    public int Power => Reds * Blues * Greens;
+    
+    public void AddIfHigher(string color, int count)
+    {
+        switch (color)
+        {
+            case "blue" when count > Blues:
+                Blues = count;
+                return;
+            case "green" when count > Greens:
+                Greens = count;
+                return;
+            case "red" when count > Reds:
+                Reds = count;
+                return;
+        }
+    }
+
+    public bool IsValid(int reds, int greens, int blues)
+    {
+        return reds >= Reds && greens >= Greens && blues >= Blues;
+    }
+}
