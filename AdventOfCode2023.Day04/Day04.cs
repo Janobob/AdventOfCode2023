2022b12d6832820345660486bd14f5a7c7ca04d5
Jan Werren <jan.werren@outlook.com>
2024-01-22 21:56:06 +0100

    feat: day four part 1

---
+++ AdventOfCode2023.Day04/Day04.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var content = File.ReadAllLines("input.txt");
+var total = 0;
+
+foreach (var line in content)
+{
+    var parsed = line.Split(":")[1].Split("|");
+    var winningsNumbers = parsed[0].Trim().Split(" ").Where(x => x != "").Select(x => Convert.ToInt32(x)).ToList();
+    var numbers = parsed[1].Trim().Split(" ").Where(x => x != "").Select(x => Convert.ToInt32(x)).ToList();
+
+    total += (int)Math.Pow(2, winningsNumbers.Count(x => numbers.Contains(x)) - 1);
+}
+
+Console.WriteLine(total);
